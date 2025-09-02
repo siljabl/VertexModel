47720c9ebbc56f5ecf802fc9799e43c70b703516
silja uio <s.b.lastad@mn.uio.no>
2025-09-02 16:52:56 +0200

    starting correlation analysis

---
+++ analysis/compute_correlations.py
@@ -0,0 +1,9 @@
+from cells.bind import VertexModel
+
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+from operator import itemgetter
+from utils import plot_utils
+from utils.correlations import general_spatial_correlation, general_temporal_correlation
