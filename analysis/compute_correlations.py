5138dcb64b84fb084f56fba1dfc9733b00d0e2bd
silja uio <s.b.lastad@mn.uio.no>
2025-09-02 18:45:07 +0200

    finished script

--- analysis/compute_correlations.py
+++ analysis/compute_correlations.py
@@ -1,12 +1,7 @@
-import os, sys
-print(os.path.dirname(sys.executable))
-
 from cells.bind import VertexModel
 
-import pickle
 import numpy as np
-import matplotlib
-matplotlib.use('Agg')
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from pathlib import Path
@@ -14,17 +9,28 @@ from operator import itemgetter
 import utils.vm_output_handling as output
 from utils.correlations import general_spatial_correlation, general_temporal_correlation
 
+# turn off interactive plotting
+mpl.use('Agg')
+
 dr    = 2
 r_max = 20
 
 
-fig, ax = plt.subplots(1,1, figsize=(5,3))
+fig, ax = plt.subplots(1,3, figsize=(10,3))
+for i in range(3):
+    ax[i].hlines(0, 0, 18, linestyle='dashed', color="gray")
 
-for file in Path("data/simulated/raw/").glob("nodivision_20250902_1328.p"):
+cmap = plt.get_cmap('plasma', 6)
+
+i = 1
+for file in Path("data/simulated/raw/").glob("nodivision_20250902_*.p"):
     print(file)
 
     # load frames
     list_vm, init_vm = output.load(file)                       # stop when we have read the whole file
+
+    # get compression/extension
+    rho = np.round(42 / init_vm.systemSize[0], 1)
 
     # get cell properties
     positions = output.get_cell_positions(list_vm)
@@ -41,11 +47,21 @@ for file in Path("data/simulated/raw/").glob("nodivision_20250902_1328.p"):
 
     # compute correlation
     C_hh = general_spatial_correlation(positions[:,:,0], positions[:,:,1], h_var, dr=dr, r_max=r_max, t_avrg=True)
-    C_AA = general_spatial_correlation(positions[:,:,0], positions[:,:,1], A_var, dr=dr, r_max=r_max)
-    C_VV = general_spatial_correlation(positions[:,:,0], positions[:,:,1], V_var, dr=dr, r_max=r_max)
+    C_AA = general_spatial_correlation(positions[:,:,0], positions[:,:,1], A_var, dr=dr, r_max=r_max, t_avrg=True)
+    C_VV = general_spatial_correlation(positions[:,:,0], positions[:,:,1], V_var, dr=dr, r_max=r_max, t_avrg=True)
 
 
-    ax.plot(C_hh['r_bin_centers'], C_hh['C_norm'])
+    ax[0].plot(C_hh['r_bin_centers'] / rho, C_hh['C_norm'], color=cmap(i), label=rho)
+    ax[1].plot(C_AA['r_bin_centers'] / rho, C_AA['C_norm'], color=cmap(i))
+    ax[2].plot(C_VV['r_bin_centers'] / rho, C_VV['C_norm'], color=cmap(i))
 
-fig.savefig("test.png")
+    i += 1
+
+ax[0].set(xlabel=r"$r~/~r_0$", title=r"$C_{hh}(r)$")
+ax[1].set(xlabel=r"$r~/~r_0$", title=r"$C_{AA}(r)$")
+ax[2].set(xlabel=r"$r~/~r_0$", title=r"$C_{VV}(r)$")
+
+ax[0].legend()
+fig.tight_layout()
+fig.savefig("results/spatial_correlations_20250902.png")
 
