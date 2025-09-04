startet adding velocities. correlation is computed along wrong axis

analysis/compute_correlations.py
@@ -10,7 +10,7 @@ from utils.correlation_object import VMAutocorrelationObject
 
 # command-line argument parsing
 parser = argparse.ArgumentParser(description="Compute correlations on simulation data and save as pickle")
-parser.add_argument('fpattern',    type=str,   help="file patter to do computations on")
+parser.add_argument('fpattern',    type=str,   help="files to do computations on. Should be complete with *")
 parser.add_argument('--dr',        type=float, help="spatial step size [r_6]",         default='1')
 parser.add_argument('--rmax',      type=float, help="max distance to consider [r_6]",  default='20')
 parser.add_argument('--overwrite', type=bool,  help="overwrite previous computations", default=False)
@@ -29,9 +29,10 @@ for path in Path("data/simulated/raw/").glob(args.fpattern):
     rho = np.round(42 / init_vm.systemSize[0], 1)
 
     # get cell properties
-    positions = vm_output.get_cell_positions(list_vm)
-    heights   = vm_output.get_cell_heights(list_vm)
-    volumes   = vm_output.get_cell_volumes(list_vm)
+    positions  = vm_output.get_cell_positions(list_vm)
+    heights    = vm_output.get_cell_heights(list_vm)
+    volumes    = vm_output.get_cell_volumes(list_vm)
+    velocities = vm_output.get_cell_velocities(list_vm)
 
     areas = np.ma.array(volumes / heights)
 
@@ -39,6 +40,7 @@ for path in Path("data/simulated/raw/").glob(args.fpattern):
     h_variation = np.ma.array(heights - np.mean(heights, axis=1, keepdims=True), mask=False)
     A_variation = np.ma.array(areas   - np.mean(areas,   axis=1, keepdims=True), mask=False)
     V_variation = np.ma.array(volumes - np.mean(volumes, axis=1, keepdims=True), mask=False)
+    velocities  = np.ma.array(velocities, mask=False)
 
     # initialize correlation object
     autocorr_obj = VMAutocorrelationObject(fname)
@@ -48,11 +50,13 @@ for path in Path("data/simulated/raw/").glob(args.fpattern):
     autocorr_obj.compute_spatial(positions, h_variation, args.dr, args.rmax, 'hh', t_avrg=True, overwrite=args.overwrite)
     autocorr_obj.compute_spatial(positions, A_variation, args.dr, args.rmax, 'AA', t_avrg=True, overwrite=args.overwrite)
     autocorr_obj.compute_spatial(positions, V_variation, args.dr, args.rmax, 'VV', t_avrg=True, overwrite=args.overwrite)
+    autocorr_obj.compute_spatial(positions, velocities,  args.dr, args.rmax, 'vv', t_avrg=True, overwrite=args.overwrite)
 
     # compute temporal autocorrelations
     autocorr_obj.compute_temporal(h_variation, tmax, 'hh', t_avrg=True, overwrite=args.overwrite)
     autocorr_obj.compute_temporal(A_variation, tmax, 'AA', t_avrg=True, overwrite=args.overwrite)
     autocorr_obj.compute_temporal(V_variation, tmax, 'VV', t_avrg=True, overwrite=args.overwrite)
+    autocorr_obj.compute_temporal(velocities,  tmax, 'vv', t_avrg=True, overwrite=args.overwrite)
 
     # save autocorrelation
     autocorr_obj.save_pickle()

analysis/utils/vm_output_handling.py
@@ -55,5 +55,15 @@ def get_cell_volumes(list_vm):
         lambda vm: itemgetter(*cells)(vm.vertexForces["surface"].volume.copy()),  # unwrapped positions of centres
         list_vm)))
 
-
     return volumes
+
+
+def get_cell_velocities(list_vm):
+    """ Get cell volumes """
+    cells = list_vm[0].getVertexIndicesByType("centre")                 # indices of cell centres (from first frame)
+
+    velocities = np.ma.array(list(map(
+        lambda vm: itemgetter(*cells)(vm.velocities.copy()),  # unwrapped positions of centres
+        list_vm)))
+
+    return velocities