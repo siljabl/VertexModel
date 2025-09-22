09c490650cb401cc989b5375a3beb3816c204474
silja uio <s.b.lastad@mn.uio.no>
2025-09-22 09:56:01 +0200

    created dev branch

--- tmp.sh
+++ tmp.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v050_taup100/ -o --dr=2
-python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup100/ -o --dr=2
-python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0150_taup100/ -o --dr=2
-python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0200_taup100/ -o --dr=2
-
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v050_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0150_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0200_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0250_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250918_N30_L64_Lambda100_v0300_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250918_N30_L64_Lambda100_v0350_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250918_N30_L64_Lambda100_v0400_taup100/ -p=hh -v=t -o --mean_var=cell
-# python analysis/compute_correlations.py data/simulated/raw/nodivision_20250918_N30_L64_Lambda100_v0450_taup100/ -p=hh -v=t -o --mean_var=cell
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup100/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup150/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup200/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup250/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup300/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup350/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup400/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup450/ --dr=2
+python analysis/compute_correlations.py ../../../../hdd_data/silja/VertexModel_data/simulated/raw/nodivision_20250917_N30_L64_Lambda100_v0100_taup500/ --dr=2
