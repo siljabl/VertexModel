c0b5bf98de55f1782a6abfb813937272943f80ff
silja uio <s.b.lastad@mn.uio.no>
2025-11-19 10:28:47 +0100

    removed reference to rexp

--- exe/nodivision.py
+++ exe/nodivision.py
@@ -107,7 +107,6 @@ def main():
     stdV0 = config['experimental']['stdV0'] * V0            # standard deviation of cell volume distribution
     Vmin  = config['experimental']['Vmin']  * V0            # lower limit on volume
     Vmax  = config['experimental']['Vmax']  * V0            # upper limit on volume
-    rexp  = config['experimental']['rhex']                  # physical side lenght of regular cell
 
     # Forces
     gamma  = config['physics']['gamma']                    # surface tension
@@ -125,7 +124,7 @@ def main():
 
 
 
-    rho = cell_density(Ngrid, Lgrid, rexp)
+    rho = cell_density(Ngrid, Lgrid)
     update_value(config_file, 'rho', rho)
     
     # Save simulation-specific config file
