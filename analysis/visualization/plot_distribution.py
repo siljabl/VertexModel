2b88e30a5acf028ddffe3d172a7b5f4c909d0ddd
silja uio <s.b.lastad@mn.uio.no>
2025-11-27 10:45:16 +0100

    adding distribution scripts

--- analysis/visualization/plot_distribution.py
+++ analysis/visualization/plot_distribution.py
@@ -1,201 +1,168 @@
-from cells.bind import VertexModel
-
 import os
 import sys
-import glob
+import pickle
 import argparse
 import numpy as np
-from pathlib import Path
-
-# Append the path of relative_parent directories
-sys.path.append("analysis/")
-sys.path.append("VertexModel/analysis/")
-
-# Avoid localhost error on my machine
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-mpl.use('Agg')
+mpl.use("Agg")
 
-import utils.config_functions as     config
-from utils.path_handling      import decompose_input_path
-from utils.correlation_object import VMAutocorrelationObject
+from pathlib import Path
+from cmcrameri import cm
+
+# # Append the path of relative_parent directories
+# sys.path.append("code/analysis/utils")
+# from data_class import SegmentationData
+# from variation_functions import global_density
+
+# sys.path.append("code/preprocessing/utils")
+# from segment2D import get_pixel_size
+
+sys.path.append("analysis/utils")
+import config_functions   as config
+import vm_output_handling as vm_output
 
 
-# Define paths
+
+def hist_to_curve(arr, bins=0, hist_range=None):
+    if hist_range == None:
+        hist_range = (np.ma.min(arr), np.ma.max(arr))
+
+    if bins == 0:
+        bins  = int(np.max(arr))
+
+    y, x = np.histogram(arr, bins=bins, range=hist_range, density=True)
+
+    return 0.5*(x[1:] + x[:-1]), y, bins
+
+
+
+obj_dir    = "data/simulated/processed/"
+data_dir   = "data/simulated/raw/"
 config_dir = "data/simulated/configs/"
 
 
-def sort_files(fpattern, legend):
-    """ 
-    Goes through files to plot and returns sorted arrays of legend labels and files
+parser = argparse.ArgumentParser(description='Plot data set')
+parser.add_argument('path',            type=str,   help="Path to file to plot. Typically 'data/experimental/processed/<dataset>")
+parser.add_argument('--bin_size',     type=int, help="data set as listed in holo_dict", default=200)
+parser.add_argument('-b', '--binned',  action="store_true", help="bin data by density")
+parser.add_argument('-r', '--rescale', action="store_true", help="rescale data")
+parser.add_argument('--ylog',          action="store_true", help="rescale data")
+args = parser.parse_args()
 
-    Parameters:
-    - fnames: file name pattern. File names on form <fnames>*.autocorr
-    - legend: key in config that is used to label plot. Also used as title on legend.
-    """
+# load experimental distribution
 
-    file_list  = []
-    label_list = []
+#######################
+# import and bin data #
+#######################
 
-    # Aquire labels from config
-    for path in glob.glob(f"{fpattern}*.autocorr"):
+# Load config
 
-        fname = Path(path).stem
-        if 'seed' in fname:
-            fname = Path(path).parent.stem
+config_path = f"{config_dir}{Path(args.path).stem}.json"
+config_file = config.load(config_path)
 
-        # Load config to get plot label
-        config_path = f"{config_dir}/{fname}.json"
-        config_file = config.load(config_path)
+# Compute time period between frames
+dt = config_file["simulation"]["dt"]
+T  = config_file["simulation"]["period"]
+df = T * dt
 
-        # Get values from
-        label = config.get_value(config_file, legend)
+# Load frames as vm objects
+list_vm, init_vm = vm_output.load(args.path, df=df)
 
-        # save in arrays
-        file_list.append(path)
-        label_list.append(label)
-
-    # Sort labels if legend is specified
-    if legend != '':
-        
-        # Sort according to label value
-        sorted_inds = np.argsort(label_list)
-        sorted_file_list  = np.array(file_list)[sorted_inds]
-        sorted_label_list = np.array(label_list)[sorted_inds]
-
-        return sorted_file_list, sorted_label_list
-    
-    else:
-        return file_list, label_list
+#cell_heights    = vm_output.get_cell_heights(list_vm)
+#cell_areas      = vm_output.get_cell_areas(list_vm)
+cell_velocities = vm_output.get_cell_velocities(list_vm)
+# cell_shapes
 
 
-
-def initialize_figure(varname, type):
-    """ Create figure """
-
-    fig = plt.figure(figsize=(6, 4), dpi=300)
-
-    if type == 'r':
-        plt.title(rf'$C_{{{varname}}}(r)$')
-        plt.xlabel(r'$r~/~r_6^*$')
-        plt.axhline(0, 0, 1, linestyle="dashed", color="gray")
-
-    else:
-        plt.title(rf'$C_{{{varname}}}(t)$')
-        plt.xlabel(r'$t~/~\tau_p$')
-        plt.axhline(0, 0, 1, linestyle="dashed", color="gray")
-
-    return fig
+######################
+# plot distributions #
+######################
 
 
+plt.hist(cell_velocities)
+plt.savefig("results/test.png", dpi=300)
 
-def save_plot(figure, out_path):
-    """ Save the generated plot to a specific directory. """
-    figure.savefig(out_path)
-    print(f"Plot saved to {out_path}")
+# N = len(height)
+# colors  = cm.roma_r(np.linspace(0, 1, N))
+# hlow, hhigh = 0, 12
+# Alow, Ahigh = 0, 1500
+# Vlow, Vhigh = 0, 6000
+# bins = 20
 
+# fig, ax = plt.subplots(1,3, figsize=(12,3.5))
 
-def plot_correlation(path, label, color, args):
-    # Load data
-    corr_obj = VMAutocorrelationObject(out_path=path)
+# for i in range(len(height)):
 
-    # Load config
-    fname = Path(path).stem
-    if 'seed' in fname:
-        fname = Path(path).parent.stem
-        fig_dir = f"results/{fname}/"
-        Path(f"{fig_dir}").mkdir(parents=True, exist_ok=True)
+#     if args.rescale:
+#         height[i] = (height[i] - np.ma.mean(height[i], keepdims=True)) / np.ma.std(height[i], keepdims=True)
+#         area[i]   = (area[i]   - np.ma.mean(area[i],   keepdims=True)) / np.ma.std(area[i],   keepdims=True)
+#         volume[i] = (volume[i] - np.ma.mean(volume[i], keepdims=True)) / np.ma.std(volume[i], keepdims=True)
 
-    config_path = f"{config_dir}{fname}.json"
-    config_file = config.load(config_path)
+#         hlow, hhigh = -3, 5
+#         Alow, Ahigh = -3, 5
+#         Vlow, Vhigh = -3, 5
 
-    # Plot
-    if args.var == "r":
-        x = corr_obj.r_array[args.param]
-        y = corr_obj.spatial[args.param]
-        plt.plot(x[(x <= args.xlim) * (y <= args.ylim)], y[(x <= args.xlim) * (y <= args.ylim)],
-                    args.fmt,
-                    color=color,
-                    label=label)
-    
-    else:
-        # Get persistence time
-        taup = config.get_value(config_file, 'taup')
-        x = corr_obj.t_array[args.param] / taup
-        y = corr_obj.temporal[args.param]
-        plt.plot(x[(x <= args.xlim) * (y <= args.ylim)], y[(x <= args.xlim) * (y <= args.ylim)],
-                    args.fmt,
-                    color=color, 
-                    label=label)
+#         bins = 20
+
+#     h_x, h_y, bins = hist_to_curve(height[i], bins=bins, hist_range=(hlow, hhigh))
+#     A_x, A_y, bins = hist_to_curve(area[i],   bins=bins, hist_range=(Alow, Ahigh))
+#     V_x, V_y, bins = hist_to_curve(volume[i], bins=bins, hist_range=(Vlow, Vhigh))
+#     #p_x, p_y, bins = hist_to_curve(shape[i],  bins=30)
+
+#     ax[0].plot(h_x, h_y, '-', color=colors[i])
+#     ax[1].plot(A_x, A_y, '-', color=colors[i])
+#     ax[2].plot(V_x, V_y, '-', color=colors[i])
+#     #ax[1,1].plot(p_x, p_y, '-', color=colors[i])
+
+   
+# if args.rescale:
+#     ax[0].set(xlabel=r"$(h-\bar{h}) ~/~ \sigma_h$",   ylabel="PDF")
+#     ax[1].set(xlabel=r"$(A-\bar{A}) ~/~ \sigma_A$")
+#     ax[2].set(xlabel=r"$(V-\bar{V}) ~/~ \sigma_V$")
+
+# else:
+#     ax[0].set(xlabel=r"$h ~[µm]$",   ylabel="PDF")
+#     ax[1].set(xlabel=r"$A ~[µm^2]$")
+#     ax[2].set(xlabel=r"$V ~[µm^3]$")
+
+# if args.ylog:
+#     for i in range(3):
+#         ax[i].set(yscale="log")
+
+# fig.tight_layout()
+# fig.subplots_adjust(right=0.85)
+
+# cbar_ax = fig.add_axes([0.88, 0.15, 0.02, 0.7])
+# sm      = plt.cm.ScalarMappable(cmap=cm.roma_r, norm=plt.Normalize(vmin=density.min(), vmax=density.max()))
+
+# fig.colorbar(sm, cax=cbar_ax, label=r"$\rho_{cell}(t) ~[mm^{-2}]$")
+# fig.savefig(f"results/{dataset}/height_area_volume_distributions.png", dpi=300)
 
 
 
-
-def main():
-    fig_dir = "results/"
-
-    parser = argparse.ArgumentParser(description="Plot all defined autocorrelations")
-    parser.add_argument('filepattern',   type=str, help="Path to files to plot. Typically 'data/simulated/obj/file'. Filename is on form <'path/to/file'>*.autocorr")
-    parser.add_argument('param',         type=str, help="Parameter to plot correlation of (varvar)")
-    parser.add_argument('var',           type=str, help="Correlation variable (t or r)")
-    parser.add_argument('-l','--legend', type=str, help="Add legend (str)",                         default='')
-    parser.add_argument('-c','--cmap',   type=str, help="Specify matplotlib colormap (str)",        default='plasma')
-    parser.add_argument('-o','--outdir', type=str, help="Output directory",                         default="results/")
-    parser.add_argument('-x', '--xlim',  type=float, help="Upper limit on x-axis", default=9999)
-    parser.add_argument('-y', '--ylim',  type=float, help="Upper limit on x-axis", default=1.1)
-    parser.add_argument('--xlog', action="store_true")
-    parser.add_argument('--ylog', action="store_true")
-    parser.add_argument('--log',  action="store_true")
-    parser.add_argument('--fmt',    default="-")
-    parser.add_argument('-r', '--return_plot', action="store_true")
-    args = parser.parse_args()
-
-    # Assert temporal or spatial correlation
-    assert args.var in ['r', 't'], "Wrong correlation variable. Must be r or t"
-
-
-    # Sort data sets by legend value
-    files_list, labels_list = sort_files(args.filepattern, args.legend)
-
-    assert len(files_list) > 0, f"No files matches filename: {args.filepattern}*.autocorr"
-
-
-    # Define line colors
-    cmap   = mpl.colormaps[args.cmap]
-    colors = cmap(np.linspace(0.1, 0.9, len(files_list)))
-
-
-    # Plot each data set
-    fig = initialize_figure(args.param, args.var)
-    for file, label, color in zip(files_list, labels_list, colors):
-
-        plot_correlation(file, label, color, args)
-        
-    # Add legend
-    if args.legend != '':
-        fig.legend(title=rf'${args.legend}$')
-
-    if args.var == "r":
-        out_path = f"{fig_dir}spatial_autocorrelation_{args.param}.png"
-    else:
-        out_path = f"{fig_dir}temporal_autocorrelation_{args.param}.png"
-
-    # log-scale
-    if args.xlog or args.log:
-        fig.xscale("log")
-    if args.ylog or args.log:
-        fig.yscale("log")
-
-    if args.return_plot:
-        return fig
-
-    else:
-        fig.tight_layout()
-        fig.savefig(out_path)
-        print(f"Plot saved to {out_path}")
+# # ##################
+# # # Save as pickle #
+# # ##################
+# # out_dict = {'area':    area,
+# #             'height':  height,
+# #             'volume':  volume,
+# #             'shape':   shape,
+# #             'density': density_bins}
 
 
 
-if __name__ == "__main__":
-    main()
+# # # if not given, use input folder for outpur also
+# # if args.out_path == None:
+# #     args.out_path = args.in_path
+
+# # # create figure folder
+# # try:
+# #     os.mkdir(f"{args.out_path}/figs")
+# # except:
+# #     None
+
+# # # save as pickle
+# # with open(f"{args.out_path}/hAV_distributions.pkl", 'wb') as handle:
+# #     pickle.dump(out_dict, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
