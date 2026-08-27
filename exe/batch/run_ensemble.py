ee3bff697640c64549ad6eaaa4e60a44230155c0
siljabl <siljabl@github.com>
2026-08-27 15:45:42 +0200

    restructures folder hierachy

--- exe/batch/run_ensemble.py
+++ exe/batch/run_ensemble.py
@@ -28,7 +28,7 @@ def main():
     # Command-line argument parsing
     parser = argparse.ArgumentParser(description="Run several runs")
     parser.add_argument('script',         type=str,  help='Simulation script')
-    parser.add_argument('-N', '--Nruns',  type=int,  help="Number of runs to so",         default=4)
+    parser.add_argument('-N', '--Nruns',  type=int,  help="Number of runs to so",         default=10)
     parser.add_argument('-P', '--Npool',  type=int,  help="Number of parallel processes", default=16)
     parser.add_argument('-s', '--seed',   type=int,  help="Simulation seed",              default=None)
     parser.add_argument('-c', '--config', type=str,  help='Path to config file',          default='configs/base/config_constant_volume.json')
