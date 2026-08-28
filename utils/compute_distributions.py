45d8284193ea5bad6ac4e05e526a894fc34e5be8
siljabl <siljabl@github.com>
2026-08-28 16:30:57 +0200

    added dataset mean and std

--- utils/compute_distributions.py
+++ utils/compute_distributions.py
@@ -1,7 +1,28 @@
 import numpy as np
 
+def dataset_mean(arr):
+    dims = np.shape(arr)
 
-def rescaled_population_mean(arr):
+    if len(dims) == 3:
+        mean = np.ma.mean(arr, axis=(0,2))
+    else:
+        mean = np.ma.mean(arr)
+
+    return mean
+
+
+def dataset_std(arr):
+    dims = np.shape(arr)
+
+    if len(dims) == 3:
+        std = np.ma.std(arr, axis=(0,2))
+    else:
+        std = np.ma.std(arr)
+
+    return std
+
+
+def timepoint_mean(arr):
     dims = np.shape(arr)
 
     if len(dims) == 3:
@@ -9,7 +30,7 @@ def rescaled_population_mean(arr):
     else:
         mean = np.ma.mean(arr, axis=1, keepdims=True)
 
-    return arr / mean
+    return  mean
 
 
 def hist_to_curve(arr, bins=None, hist_range=None):
@@ -27,7 +48,7 @@ def hist_to_curve(arr, bins=None, hist_range=None):
 
 def rescaled_distribution(arr, bins=None):
 
-    arr_rescaled = rescaled_population_mean(arr)
+    arr_rescaled = arr / timepoint_mean(arr)
     arr_rescaled = arr_rescaled.flatten()
     obs_rescaled, freq, _ = hist_to_curve(arr_rescaled, bins=bins)
 
