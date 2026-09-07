3af6f040803ed1dbab0c4f1bd832a05184dca47c
siljabl <siljabl@github.com>
2026-09-07 10:12:14 +0200

    rhex -> rgrid

--- utils/vm_geometry.py
+++ utils/vm_geometry.py
@@ -1,17 +1,17 @@
 
 
-def hexagon_volume(rhex):
-    """ Computes volume of regular hexagon with side length rhex  """
+def hexagon_volume(rgrid):
+    """ Computes volume of regular hexagon with side length rgrid  """
 
-    return (3**2 / 2) * rhex**3
+    return (3**2 / 2) * rgrid**3
 
 
-def hexagon_area(rhex):
-    """ Computes area of regular hexagon with side length rhex """
+def hexagon_area(rgrid):
+    """ Computes area of regular hexagon with side length rgrid """
 
-    return (3**(3/2) / 2) * (rhex)**2
+    return (3**(3/2) / 2) * (rgrid)**2
 
 def hexagon_side(V0):
-    """ Computes side length of regular hexagon from volume """
+    """ Computes side length of regular hexagon fm volume """
 
     return ((2 / 3**2) * V0) ** (1/3)
