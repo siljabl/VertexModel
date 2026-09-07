8cd8363968b6df5a0430e34693f9a9a69c5d4124
siljabl <siljabl@github.com>
2026-09-07 10:18:15 +0200

    Removed duplicate code

--- utils/vm_calibration.py
+++ utils/vm_calibration.py
@@ -1,26 +1,6 @@
 import numpy as np
 
 
-def cell_density(config):
-    """
-    Global cell density (cells per unit area) from hexagonal grid setup.
-    Output is in units of cells per mm^2
-
-    Ngrid: number of vertices per side in the hexagonal lattice
-    Lgrid: system size in the same length units as positions
-    """
-    Ngrid = config['simulation']['Nvertices']
-    Lgrid = config['simulation']['Lgrid']
-
-
-    Ncell = Ngrid**2 / 3.0
-    Agrid = (np.sqrt(3) / 2.0) * Lgrid**2
-
-    rho = Ncell / Agrid
-
-    return int(rho * 10**6)
-
-
 def cell_density_from_Ngrid(Ngrid, Lgrid=600):
     """
     Global cell density (cells per unit area) from hexagonal grid setup.
@@ -30,12 +10,29 @@ def cell_density_from_Ngrid(Ngrid, Lgrid=600):
     Lgrid: system size in the same length units as positions
     """
 
+    # Number of cells in lattice
     Ncell = Ngrid**2 / 3.0
+
+    # Total area of hexagonal lattice
     Agrid = (np.sqrt(3) / 2.0) * Lgrid**2
 
+    # Cells per unit area
     rho = Ncell / Agrid
 
-    return np.astype(rho * 10**6, int)
+    # Convert to cells per mm^2 (assuming Lgrid is in µm)
+    return int(rho * 10**6)
+
+
+
+def cell_density(config):
+    """
+    Convenience: global cell density (cells per unit area) from config.
+    """
+    Ngrid = config['simulation']['Nvertices']           # Number of vertices in each dimension
+    Lgrid = config['simulation']['Lgrid']               # Length of lattice in µm
+
+    return cell_density_from_Ngrid(Ngrid, Lgrid)
+
 
 
 def cell_volume_from_density(rho):
@@ -50,17 +47,21 @@ def cell_volume_from_density(rho):
 
 def cell_volume(config):
     """
-    Convenience: average cell volume from (Ngrid, Lgrid) by
+    Convenience: average cell volume from config by
     combining cell_density and cell_volume_from_density.
     """
     rho = cell_density(config)
+
     return cell_volume_from_density(rho)
 
 
 def cell_division_volume(config):
-
+    """
+    Threshold volume for cell division, as a ratio of the average cell volume.
+    """
     Vth_ratio = config["calibration"]["Vth_ratio"]
-
+    
+    # Experimental mean cell volume (from calibration / density)
     V0 = cell_volume(config)
 
     return Vth_ratio * V0
